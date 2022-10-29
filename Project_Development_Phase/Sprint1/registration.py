ad88d4f2067da43b2c31ab46d60d26242d7c43bc
Preethaa J <preethaaj.io@gmail.com>
2022-10-30 01:59:40 +0530

    add authentication

--- Project_Development_Phase/Sprint1/registration.py
+++ Project_Development_Phase/Sprint1/registration.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
 
 # Create an empty container
 placeholder = st.empty()
@@ -13,6 +15,18 @@ with placeholder.form("login"):
     password = st.text_input("Password", type="password")
     submit = st.form_submit_button("Login")
 
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    st.write(f'Welcome *{name}*')
+    st.title('Some content')
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
+
+"""
 if submit and email == actual_email and password == actual_password:
     # If the form is submitted and the email and password are correct,
     # clear the form/container and display a success message
@@ -22,3 +36,4 @@ elif submit and email != actual_email and password != actual_password:
     st.error("Login failed")
 else:
     pass
+"""
